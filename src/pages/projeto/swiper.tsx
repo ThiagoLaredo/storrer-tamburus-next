7c89e95e1a390d449b2b5ef1de495e636a7d56be
ThiagoLaredo <thiago@olatu.com.br>
2025-10-05 15:52:24 -0300

    inicio

--- src/pages/projeto/swiper.tsx
+++ src/pages/projeto/swiper.tsx
@@ -1,39 +1,4 @@
-"use client";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Mousewheel, Keyboard } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
-import Image from "next/image";
-import Link from "next/link";
-
-type Projeto = {
-  title: string;
-  slug: string;
-  capa: string;
-};
-
-type Props = { projetos: Projeto[] };
-
-export default function ProjetosSwiper({ projetos }: Props) {
-  return (
-    <Swiper
-      direction="vertical"
-      modules={[Pagination, Mousewheel, Keyboard]}
-      pagination={{ clickable: true }}
-      mousewheel
-      keyboard
-      className="projetos-swiper"
-    >
-      {projetos.map(p => (
-        <SwiperSlide key={p.slug}>
-          <Image src={p.capa} alt={p.title} fill className="object-cover" />
-          <div className="overlay">
-            <Link href={`/projetos/${p.slug}`}>
-              <h3>{p.title}</h3>
-            </Link>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+// src/pages/projeto/swiper.tsx
+export default function SwiperPlaceholder() {
+  return <div>Página em desenvolvimento</div>;
 }
